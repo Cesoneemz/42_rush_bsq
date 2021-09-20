fe9f8fd749f5a152b5f7b5d2cce973f8ba75b13f
wlanette <wlanette@student.21-school.ru>
2021-09-20 19:53:56 +0300

    initial

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Rush_BSQ
+//
+//  Created by Wylde Lanette on 9/20/21.
+//  Copyright © 2021 Wylde Lanette. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
